1f75f5a6e47c15178be30a649a950144646df0f0
Josh Caron <jcaron@ccs.neu.edu>
2014-01-25 03:45:28 -0500

    Changed prereqs to be lists of lists

--- mongo_import_script.py
+++ mongo_import_script.py
@@ -1,5 +1,6 @@
 import json
 import pymongo
+import re
 
 # Mongo setup
 from pymongo import MongoClient
@@ -11,12 +12,38 @@ collection = db.courses
 json_data = open('all_data_new.json')
 data = json.load(json_data)
 
+
+
+
+# Get a list of prerequisites for the given course
+def get_prereq(prereqstr):
+	prereq_list = []
+	if (prereqstr):
+		prereqs = prereqstr.split("Coreq")[0]
+		ands = prereqs.split(" and")
+		for prereq in ands:
+			ors = re.findall("[A-Z]{2,4} [0-9]{4}", prereq)
+			if (len(ors) == 0):
+				continue
+			else:
+				prereq_list.append(ors)
+
+		return prereq_list
+
+	return prereq_list
+
+
+
+
+
+
 for department, courses in data.iteritems():
 
 	for course in courses:
 		mongoCourse = {}
 		mongoCourse["departmentId"] = department
 		for id, courseData in course.iteritems():
+			courseData["prereqstr"] = get_prereq(courseData["prereqstr"])
 			mongoCourse["courseId"] = id
 			mongoCourse["courseData"] = courseData
 		collection.insert(mongoCourse)
