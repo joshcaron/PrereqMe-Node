1c1d8c9163d7cb5920ca97cdb8271b5b2a218566
Josh Caron <jcaron@ccs.neu.edu>
2014-01-25 14:18:06 -0500

    Recursive prerequisite lookup

--- www/api.js
+++ www/api.js
@@ -12,14 +12,8 @@ var Class = function(data) {
 	self.dept = data.departmentId;
 	self.num = data.courseId;
 	self.name = data.courseData.title;
-	// self.prereqs = data.courseData.prereqstr;
-	var prereqObjects = [];
-	data.courseData.prereqstr.forEach(function(prereqs) {
-		prereqObjects.push(makePrereqs(prereqs));
-		// console.log(makePrereqs(prereqs));
-	});
-	self.prereqs = prereqObjects;
-	// console.dir(self.prereqs);
+	self.prereqs = data.courseData.prereqstr;
+	self.coreqs = null;
 	return self;
 }
 
@@ -28,16 +22,219 @@ function courseToClass(course) {
 }
 
 
+function debug(data) {
+	// console.log("DEBUG: " + data);
+}
 
-// function makePrereqs(prereqList) {
-// 	var prereqs = [];
-// 	prereqList.forEach(function(prereq) {
-// 		var pieces = prereq.split(" ");
-// 		var deptId = pieces[0];
-// 		var courseId = parseInt(pieces[1]);
-// 		courseLookup.find(deptId, courseId, prereqs);
-// 	});
-// }
+/**
+ * Loads a course asynchronously
+ * @param  {String}   deptId   The department id of the course
+ * @param  {Number}   courseId The course id
+ * @param  {Function} callback A callback function specified by the signature:
+ *                             	function(success, data) { .... }
+ */
+function loadAsyncCourse(deptId, courseId, callback) {
+	Course.find({"departmentId" : deptId}, function(err, courses) {
+		if (err) {
+			console.log(err);
+			callback(false, err);
+		}
+		var foundCourse = null;
+		// Find the course in the department
+		courses.forEach(function(course) {
+			if (course.courseId == courseId) {
+				foundCourse = course;
+			}
+		});
+		if (!foundCourse) {
+			callback(false, "Did not find: " + deptId + " " + courseId);
+		} else {
+			callback(true, foundCourse);
+		} 
+	});
+}
+
+/**
+ * Loads a list of courses asynchronously
+ * @param  {Array} courses      The list of courses as strings "XXXX 0000"
+ * @param  {Function} listCallback The callback function to call when the list has loaded, 
+ *                                 specified by the signature:
+ *                                 	function(success, data) { ... }
+ */
+function loadAsyncCourseList(courses, listCallback) {
+	var courseData = [];
+
+	// // DEBUG
+	// console.log("Loading course list....");
+	// console.log(courses);
+
+	async.forEach(courses, function(course, courseLoadedCallback) {
+		var pieces = course.split(" ");
+		var deptId = pieces[0];
+		var courseId = pieces[1];
+		var loadCourse = function(success, data) {
+			if (!success) {
+				console.log("ERROR: " + data + "\n");
+			}
+
+			// DEBUG
+			// console.log("Loaded a course.");
+			// console.log(data);
+			
+			// courseData.push(data);
+			var loadPrereqs = function(success, prereqData) {
+				if (!success) {
+					console.log(prereqData);
+				} else {
+					courseData.push(prereqData);					
+				}
+				courseLoadedCallback();
+			}
+			if (data && data.courseData && data.courseData.prereqstr) {
+				loadAsyncCoursePrereqs(data, loadPrereqs);				
+			} else {
+				courseLoadedCallback();
+			}
+		}
+		loadAsyncCourse(deptId, courseId, loadCourse);
+	}, function(err) { // Finished loading all courses in the list
+		if (err) {
+			console.log(data);
+			listCallback(false, err);
+		}
+		listCallback(true, courseData);
+	});
+}
+
+/**
+ * Loads the preqreqs of a course asynchronously
+ * @param  {Object}   course   The raw course data
+ * @param  {Function} callback The callback for after the information has loaded for the course
+ */
+function loadAsyncCoursePrereqs(course, callback) {
+	var prereqList = course.courseData.prereqstr;
+	var prereqs = [];
+	// DEBUG
+	// console.log("Loading prereqs for " + course.departmentId + " " + course.courseId);
+
+	async.forEach(prereqList, function(prereqGroup, prereqGroupLoaded) {
+		var loadGroup = function(success, data) {
+			if (!success) {
+				console.log(data);
+			}
+			prereqs.push(data);
+			prereqGroupLoaded();
+		}
+		loadAsyncCourseList(prereqGroup, loadGroup);
+
+	}, function(err) { // All prereqs loaded
+		if (err) {
+			console.log(err);
+			callback(false, err);
+		} else {
+			course.preqreqs = prereqs;
+			callback(true, course);
+		}
+
+	});	
+}
+
+
+function loadAsyncRoot(deptId, courseId, callback) {
+	var course = null;
+	var root = null;
+	var localData = {
+		root: null,
+		prereqs: []
+	}
+	debug("Loading the root " + deptId + " " + courseId);
+	async.series([
+		// First, we load the root course
+		function(rootLoaded) {
+			// When the root has loaded
+			var rootLoad = function(success, data) {
+				if (!success) {
+					console.log("ERROR: " + data);
+				} else {
+					debug("Loaded root " + deptId + " " + courseId + "\n");
+					root = data;
+					localData.root = new Class(data);
+				}
+				rootLoaded();
+			};
+
+			loadAsyncCourse(deptId, courseId, rootLoad);
+		},
+
+		// After root has been loaded, we load the prereqs
+		function(allPreqreqsLoaded) {
+			// Make sure there are prereqs
+			debug("Checking prereqs for " + deptId + " " + courseId);
+			if (root && root.courseData && root.courseData.prereqstr && root.courseData.prereqstr.length > 0) {
+				// Get all the prereq groups
+				debug("Prereqs found.");
+				var prereqList = root.courseData.prereqstr;
+				debug("Prereqs are: " + prereqList);
+				debug("Getting prereqs...");
+				var prereqs = [];
+				async.forEach(prereqList, function(prereqGroup, groupComplete) {
+					var loadGroup = function(success, data) {
+						if (!success) {
+							console.log("ERROR: " + data);
+							groupComplete();
+						} else {
+							// Loaded the group
+							// Now, load the root course for each of them.
+							var oldPrereqGroup = data;
+							var newPrereqGroup = [];
+							async.forEach(oldPrereqGroup, function(prereq, newRootComplete) {
+								var pDeptId = prereq.departmentId;
+								var pCourseId = prereq.courseId;
+								var loadNewRoot = function(success, data) {
+									if (!success) {
+										console.log("ERROR: " + data);
+									} else {
+										// Successfully made new roots
+										newPrereqGroup.push(data);
+									}
+									newRootComplete();
+								};
+								loadAsyncRoot(pDeptId, pCourseId, loadNewRoot);
+
+							}, function(err) { // Loaded the new roots for the prereqs
+								if (err) {
+									console.log("ERROR: " + err);
+								} else {
+									prereqs.push(newPrereqGroup);
+								}
+								debug("Prereqs loaded.");
+								groupComplete();
+							});
+						}
+					};
+					loadAsyncCourseList(prereqGroup, loadGroup);
+				}, function(err) { // All groups are complete
+					if (err) {
+						console.log("ERROR: " + err);
+					}
+					localData.prereqs = prereqs;
+					allPreqreqsLoaded();
+				});
+			} else {
+				// No prereqs, just go ahead and complete
+				debug("No prereqs found.\n");
+				allPreqreqsLoaded();
+			}
+		}
+	], function(err) { // Fully loaded the root and its prereqs
+		if (err) {
+			console.log("ERROR: " + err);
+			callback(false, err);
+		} else {
+			callback(true, localData);
+		}
+	});
+}
 
 
 var convertCourseList = function(courseList) {
@@ -53,7 +250,7 @@ var convertCourseList = function(courseList) {
 exports.setup = function(app) {
 	// The API root
 	app.get('/api', function (req, res) {
-	  res.send('API is running');
+		res.send('API is running');
 	});
 
 
@@ -70,7 +267,7 @@ exports.setup = function(app) {
 
 	// Getting courses by department id
 	app.get('/api/courses/:deptId', function(req, res) {
-		console.log(res.send);
+		// console.log(res.send);
 		Course.find( { "departmentId": req.params.deptId }, function(err, courses) {
 			if (err) {
 				console.log(err);
@@ -80,93 +277,81 @@ exports.setup = function(app) {
 		});
 	});
 
-	var serveRequest = function() {
-		return function(req, res) {
-			var sendEverything = function() {
-				return function(err, courses) {
-					if (err) {
-						console.log(err);
-					}
-					var foundCourse = null;
-					courses.forEach(function(course) {
-						if (course.courseId == req.params.courseId) {
-							foundCourse = new Class(course);
-						}
-					});
-					res.send(foundCourse);
-				}
-			}
-
-			Course.find({"departmentId": req.params.deptId}, sendEverything());
-		}
-	}
-
 	// Getting courses by department id and course id
 	app.get('/api/courses/:deptId/:courseId', function(req, res, next) {
-		var locals = {
-			root: null
-		};
 		var deptId = req.params.deptId;
 		var courseId = req.params.courseId;
-		// Execute a series of async requests
-		async.series([
-			// First, we need to get the root course
-			function(callback) {
-				// Grab all the courses in a department
-				Course.find({"departmentId" : deptId}, function(err, courses) {
-					if (err) {
-						console.log(err);
-					}
-					// Find the course in the department
-					courses.forEach(function(course) {
-						if (course.courseId == courseId) {
-							locals.root = course;
-						}
-					});
-
-					callback();
-				});
-			},
-
-			// Then, we need to get the prereqs for the root course
-			function(callback) {
-				var allPrereqs = locals.root.courseData.prereqstr;
-				var prereqs = [];
-				// For each list of prereqs, we need to fetch the AND prereqs
-				async.forEach(allPrereqs, function(preqreqList, callback) {
-					var prereqOR = [];
-					// Winthin those ANDs we need to get the ORs
-					async.forEach(preqreqList, function(prereq, callback) {
-						// Looking up a single course
-						var pieces = prereq.split(" ");
-						var pDeptId = pieces[0];
-						var pCourseId = pieces[1];
-						Course.find({"departmentId": pDeptId}, function(err, courses) {
-							if (err) {
-								console.log(err);
-							}
-							// Find the course in the department
-							courses.forEach(function(course) {
-								if (course.courseId == pCourseId) {
-									prereqOR.push(course);
-								}
-							});
-
-							callback();
-						});
-					}, function() { // Finished fetching all courses in an OR list
-						prereqs.push(prereqOR);
-						callback();
-					});
-				}, function(err) { // Finished fetching all AND lists
-					locals.prereqs = prereqs;
-					callback();
-				});
+		var loadCourses = function(success, data) {
+			if (!success) {
+				console.log("ERROR: " + data);
+			} else {
+				console.log("Fully loaded.");
+				res.send(data);
 			}
+		}
+		loadAsyncRoot(deptId, courseId, loadCourses)
+		// var locals = {
+		// 	root: null,
+		// 	prereqs: []
+		// };
+		// var deptId = req.params.deptId;
+		// var courseId = req.params.courseId;
+		// var rootCourse = null;
+		// // Execute a series of async requests
+		// async.series([
+		// 	// First, we need to get the root course
+		// 	function(callback) {
+		// 		var loadRootCourse = function(success, data) {
+		// 			// DEBUG
+		// 			console.log("LOADED ROOT");
+		// 			console.log(data);
+		// 			if (!success) {
+		// 				// ERROR :(
+		// 				console.log(data);
+		// 			}
+		// 			// We loaded the course!
+		// 			rootCourse = data;
+		// 			locals.root = {
+		// 				"dept": data.departmentId,
+		// 				"num" : data.courseId,
+		// 				"name": data.courseData.title
+		// 			}
+		// 			callback();
+		// 		}
+		// 		loadAsyncCourse(deptId, courseId, loadRootCourse);
+		// 	},
 
-		], function(err) { // Final callback
-			res.send(locals);
-		});
-		
+		// 	// Then, we need to get the prereqs for the root course
+		// 	function(callback) {
+		// 		var prereqs = [];
+				
+		// 		var prereqsToLoad = rootCourse.courseData.prereqstr;
+		// 		async.forEach(prereqsToLoad, function(prereqGroup, prereqGroupLoaded) {
+		// 			var loadPrereqs = function(success, data) {
+		// 				if (!success) {
+		// 					console.log(data);
+		// 				}
+		// 				// DEBUG
+		// 				console.log("\n\n\n\n\nLoaded prereq group.\n\n\n\n\n\n");
+		// 				console.log(data);
+
+		// 				prereqs.push(data);
+		// 				prereqGroupLoaded();
+		// 			}
+		// 			console.log("Fectching group " + prereqGroup);
+		// 			loadAsyncCourseList(prereqGroup, loadPrereqs);
+		// 		}, function(err) { // After loading all prereq groups
+		// 			if (err) {
+		// 				console.log(err);
+		// 			}
+		// 			locals.prereqs = prereqs;
+		// 			callback();
+		// 		});
+		// 	}
+
+		// ], function(err) { // Final callback
+		// 	res.send(locals);
+		// });
+
 	});
 }
