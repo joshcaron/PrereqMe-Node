fd6fb08aca249db06059f0ed6c422bf198a255d4
Josh Caron <jcaron@ccs.neu.edu>
2014-01-24 20:23:33 -0500

    Basic mongo db connection

--- www/server.js
+++ www/server.js
@@ -1,5 +1,16 @@
 var http = require("http");
+var mongoose = require('mongoose');
 
+// Mongo DB Fun times
+mongoose.connect('mongodb://localhost/test');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback () {
+  // yay!
+  console.log("YAY");
+});
+
+// Server things
 http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.write("Hello World");
